4b19666a54592a8c40ac39914aca552d19d4b82e
danielvazqueez <luis_060198@hotmail.com>
2019-10-21 17:32:19 -0500

    Add extra things

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const met = require('./met')
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
-  res.send({
-    info: 'Bienvenido a la pagina del Museo Metropolitano'
-  })
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.get('/students/:id', function(req, res) {
